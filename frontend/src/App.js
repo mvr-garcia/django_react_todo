88603615ff0d50ec6f89692ccaf2385ab758c1d0
Marcos Garcia <mvrgarcia05@gmail.com>
2021-03-30 20:54:00 -0300

    React front-end consuming data from Django back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-
-const list = [
-    {
-        "id":1,
-        "title":"My 1st ToDo",
-        "body":"This is an example ToDo to my app."
-    },
-    {
-        "id":2,
-        "title":"Learn Containers",
-        "body":"I have to learn about Containers and how to use Docker and Kubernets."
-    },
-    {
-        "id":3,
-        "title":"30' Exercise",
-        "body":"Don't foget the exercise"
-    }
-]
+import axios from 'axios';
 
 class App extends Component {
-    constructor(props)  {
-        super(props);
-        this.state = { list };
+    state =  {
+        todos: []
+    };
+
+    componentDidMount()  {
+        this.getTodos();
+    }
+
+    getTodos()  {
+        axios
+        .get('http://127.0.0.1:8000/api/')
+        .then(res => {
+            this.setState({ todos: res.data });
+        })
+        .catch(err =>   {
+            console.log(err);
+        });
     }
 
     render()    {
         return (
             <div>
-                {this.state.list.map(item => (
+                {this.state.todos.map(item => (
                     <div key={item.id}>
                         <h1>{item.title}</h1>
-                        <p>{item.body}</p>
+                        <span>{item.body}</span>
                     </div>
                 ))}
             </div>
